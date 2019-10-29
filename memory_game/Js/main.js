f65b557667cdb43eb9fcef3e668463bfd48e43ca
Jordan Trickett <jordantrickett@192-168-1-102.tpgi.com.au>
2019-10-29 18:38:00 +1100

    created game logic

---
+++ memory_game/Js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running");
+
+var cardOne = 'queen';
+var cardTwo = 'king';
+var cardThree = 'queen';
+var cardFour = 'king';
+
+console.log('user flipped ' + cardOne);
+console.log('user flipped ' + cardFour);

29ee9a9fd7364ab4e2ff011c175019ee9305a270
Jordan Trickett <jordan1trickett@gmail.com>
2019-11-01 14:48:24 +1100

    created game logic

--- memory_game/Js/main.js
+++ memory_game/Js/main.js
@@ -1,9 +1,27 @@
 console.log("up and running");
 
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'king';
+const cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log('user flipped ' + cardOne);
-console.log('user flipped ' + cardFour);
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped " + cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardTwo);
+
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1])
+	alert("you have found a match!");
+}
+
+else {
+	alert("try again!");
+}
+
+
+
+

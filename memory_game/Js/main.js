f47b8aface5085a17d8b99d7bce519f13f5a2b28
Jordan Trickett <jordan1trickett@gmail.com>
2019-11-12 15:36:33 +1100

    created game logic

--- memory_game/Js/main.js
+++ memory_game/Js/main.js
@@ -1,22 +1,48 @@
 console.log("up and running");
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+
+];
+
+
 var cardsInPlay = [];
 
 
 function checkForMatch (){
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match!");
+		alert("You found a match!");
 	}else{
-		console.log("Sorry! Try again");
+		alert("Sorry! Try again");
 	}
 };
 
 
 function flipCard (cardId){
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
